7e89c720ed122e5ab86ae52a6b43fd33eb4a014e
rusfai <70482477+rusfai@users.noreply.github.com>
2024-08-18 16:28:26 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -14,28 +14,6 @@ async def gg(user_id, msg):
 
 app = Flask(__name__)
 
-@app.route('/echo', methods=['POST'])
-def echo():
-    asyncio.get_event_loop().run_until_complete(gg(5779182088, f'ЗАПРОС ОБЫЧНЫЙ' ))
-    try:
-        id_user = request.args.get('ID')
-    except:
-        id_user = 0
-    try:
-        anower_id = request.form['ID']
-    except:
-        anower_id = 0
-    try:
-        a = request.get_json(force=True)
-    except:
-        a = 0
-    try:
-        user = jsonify(request.params)
-    except:
-        user = 0 
-
-    asyncio.get_event_loop().run_until_complete(gg(5779182088, f'ОТВЕТ {id_user} {a} {anower_id} {user}' ))
-    return a
 
 @app.route('/echo/<path>', methods=['POST'])
 def echo():
