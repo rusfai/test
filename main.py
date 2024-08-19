21d63afcab97d0d8a60b5fd59d6c4c3271185990
rusfai <70482477+rusfai@users.noreply.github.com>
2024-08-19 21:28:43 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,69 @@
 from flask import Flask, jsonify, request
 import asyncio
 from aiogram import Bot
+import mysql
+import mysql.connector
+
+admin_tg = 5779182088
+
+db_host = "93.93.207.52"
+db_user = "gen_user" 
+db_password = "PUQC7sa$"
+db_name = "default_db"
 
 token = '6188238037:AAFZcsoJGYGngbE2awHEHmtOxa2STVqkg0M'
 bot = Bot(token)
 
+def connect():
+    mydb = mysql.connector.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=db_name,
+    )
+    return mydb
 
 async def gg(user_id, msg):
     await bot.send_message(user_id, msg)
-
-
 
 
 app = Flask(__name__)
 
 @app.route('/echo', methods=['POST'])
 def echo():
-    asyncio.get_event_loop().run_until_complete(gg(5779182088, f'ЗАПРОС ОБЫЧНЫЙ' ))
+
     try:
+        asyncio.get_event_loop().run_until_complete(gg(admin_tg, f'Новая регестрация' ))
+
+
         id_user = request.args.get('ID')
-    except:
-        id_user = 0
-    try:
-        anower_id = request.form['ID']
-    except:
-        anower_id = 0
-    try:
-        a = request.get_json(force=True)
-    except:
-        a = 0
-    try:
-        user = jsonify(request.params)
-    except:
-        user = 0 
-    try:
-        b = request.args.to_dict()
-    except:
-        b = 0
+        id_user - int(id_user)
 
+        
+        mydb = connect()
+        mycursor = mydb.cursor(buffered=True)
 
-    asyncio.get_event_loop().run_until_complete(gg(5779182088, f'ОТВЕТ {id_user} {a} {anower_id} {user} {b}' ))
-    return a
+        mycursor.execute("SELECT lang FROM kwork14_user WHERE id_tg = '{}'".format(id_user))
+        lang = mycursor.fetchone()
+
+        if lang == 'ru':
+            text = 'Вы успешно зарегистрировались! Нажмите на "Главное меню", а потом "Получить сигнал"'
+        elif lang == 'en':
+            text = 'You have successfully registered! Click on the "Main Menu" and then "Receive signal"'
+        elif lang == 'tr':
+            text = 'Başarıyla kaydoldunuz! "Ana Menü" ye ve ardından "Sinyal al" a tıklayın'
+
+        mycursor.execute("UPDATE kwork14_user SET register = '{}' WHERE id_tg = '{}' ".format(1, id_user))
+        mydb.commit()
+
+        mycursor.close()
+        mydb.close()
+
+        asyncio.get_event_loop().run_until_complete(gg(int(id_user), f'{text}'))
+        return id_user
+    
+    except:
+        return
 
 
 if __name__ == '__main__':
