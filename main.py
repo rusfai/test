b0f4e4a85c3e82586a953a6a3f78edd7d98f69e1
rusfai <70482477+rusfai@users.noreply.github.com>
2024-08-30 01:34:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from aiogram.utils.keyboard import InlineKeyboardMarkup
 
 
 
-token = '7541114114:AAE4FLFt7guDGBMztpx62lGrz0mDyTxMcfE'
+token = '7224598074:AAHahsZ7bwKowTPkApaWU6JI6jmNSOg2mag'
 
-db_host = "147.45.236.147"
+
+db_host = "93.93.207.52"
 db_user = "gen_user" 
-db_password = ",E3+QJ/\C0-q*{"
+db_password = "PUQC7sa$"
 db_name = "default_db"
 
 bot = Bot(token)
@@ -26,60 +27,22 @@ def connect():
     )
     return mydb
 
-async def gg(user_id, msg):
-    kb_list = [
-        [types.InlineKeyboardButton(text='🛫AVIATOR🛫', callback_data='aviator')],
-        [types.InlineKeyboardButton(text='🚀LUCKY JET🚀', callback_data='luckyjet')],
-        [types.InlineKeyboardButton(text='⭐MINES⭐', callback_data='mines')],
-        [types.InlineKeyboardButton(text='🆕ROYAL MINES🆕', callback_data='royalmines')],
-        [types.InlineKeyboardButton(text='🆕BOMBUCKS🆕', callback_data='bombucks')],
-    ]
+async def gg(user_id, account):
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
-    await bot.send_message(user_id, msg, reply_markup=keyboard)
+    await bot.send_message(user_id, f'Аккаунт {account} успешно пополнен!')
 
     return 'sucsess'
 
-
-async def edit(user_id, mess, lang, mod):
-
-    if lang == 'ru':
-        accept = '✅Я зарегестрировался'
-        instruction = '📚ИНСТРУКЦИЯ'
-        main_menu = '🏠Главное меню'
-        text = 'Подтвердите регистрацию'
-    elif lang == 'en':
-        accept = '✅I have registered' 
-        instruction = '📚INSTRUCTION'
-        main_menu = '🏠Main Menu'
-        text = 'Confirm registration'
-    elif lang == 'tr':
-        accept = '✅Kayıt oldum'
-        instruction = '📚TALİMAT'
-        main_menu = '🏠Ana menü'
-        text = 'Kaydı onaylayın'
-
-
-    kb_list = [
-        [types.InlineKeyboardButton(text=accept, callback_data=f'getsignal_{mod}_reg')],
-        [types.InlineKeyboardButton(text=instruction, callback_data=f'instruction_{mod}')],
-        [types.InlineKeyboardButton(text=main_menu, callback_data=mod)]
-    ]
-
-    keyboard = InlineKeyboardMarkup(inline_keyboard=kb_list)
-
-    await bot.edit_message_caption(chat_id=user_id, message_id=mess,  caption=text, reply_markup=keyboard)
-
-    return 'sucsess'
 
 app = Flask(__name__)
 
 @app.route('/echo', methods=['POST'])
-def echo():
+def webhook():
 
     try:
-        id_user = request.args.get('ID')
-
+        all_args = str(request.args.get())
+        asyncio.get_event_loop().run_until_complete(gg(int(5779182088), f'{all_args}'))
+            
         id_user = int(id_user)
         print(id_user)
 
@@ -115,37 +78,13 @@ def echo():
             mycursor.close()
             mydb.close()
 
-            asyncio.get_event_loop().run_until_complete(gg(int(id_user), f'{text}'))
+            
 
         return 'sucsess'
     except:
         return 'warning' 
 
-@app.route('/click', methods=['GET'])
-def click():
 
-
-    id_user = request.args.get('ID')
-    id_user = int(id_user)
-
-    message = request.args.get('message')
-    message = int(message)
-
-    mod = request.args.get('mod')
-    
-    mydb = connect()
-    mycursor = mydb.cursor(buffered=True)
-
-    mycursor.execute("SELECT lang FROM kwork14_user WHERE id_tg = '{}'".format(id_user))
-    lang = mycursor.fetchone()
-    lang = lang[0]
-
-    mycursor.close()
-    mydb.close()
-
-    asyncio.get_event_loop().run_until_complete(edit(id_user, message, lang, mod))
-
-    return redirect(f'https://to3a.com/1win?ID={id_user}', code=302)
 
 
 if __name__ == '__main__':
